af7961bff5d8232d736a235ea9e35b8adf81d8db
VitalyVakhteev <vitaly.v@techusage.com>
2025-01-30 12:57:04 -0800

    Fix procedural generation; begin work on adding rooms

--- Assets/Scripts/Data/FloorConfig.cs
+++ Assets/Scripts/Data/FloorConfig.cs
@@ -3,14 +3,12 @@ using UnityEngine;
 [System.Serializable]
 public class FloorConfig
 {
-    public int minWidth = 8;
-    public int maxWidth = 15;
-    
+    public int minWidth  = 8;
+    public int maxWidth  = 15;
     public int minHeight = 8;
     public int maxHeight = 15;
 
-    public bool hasCornerWalls = true;
-    public bool isMainDoors = true;
-    public int stairCount = 1;
-    public int seed = 0; // For reproducible random generation
+    public bool useCornerWalls = true;
+    public bool hasDoor        = true;
+    public int stairsCount     = 1;
 }
