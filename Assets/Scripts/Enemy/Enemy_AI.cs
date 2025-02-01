e9617aa0d38d628ed6c95ab436d98df0c40ec3f9
Emil <emil.badalian@yahoo.com>
2025-01-31 17:21:07 -0800

    Organize previously added files, enemy ai should detect player when in same scene

--- Assets/Scripts/Enemy/Enemy_AI.cs
+++ Assets/Scripts/Enemy/Enemy_AI.cs
@@ -5,13 +5,12 @@ public class Enemy_AI : MonoBehaviour
     public GameObject target;
     public float moveSpeed = 1.5f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //Will find the player when they are in the same scene
+        target = GameObject.Find("Player");
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
