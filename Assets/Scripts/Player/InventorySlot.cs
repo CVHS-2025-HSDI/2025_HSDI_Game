24821e329107a81a5c7e079aa63afc23dcc28594
taratokikawa <taratokikawa@gmail.com>
2025-02-14 12:08:19 -0800

    inventory minor fixes

--- Assets/Scripts/Player/InventorySlot.cs
+++ Assets/Scripts/Player/InventorySlot.cs
@@ -3,7 +3,7 @@ using UnityEngine.EventSystems;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
-    public void onDrop(PointerEvent Data eventData){
+    public void onDrop(PointerEventData eventData){
         if(transform.childCount == 0){
             InventoryItem inventoryItem = eventData.pointerDrag.GetComponent<inventoryItem>();
             inventoryItem.parentAfterDrag = transform;
