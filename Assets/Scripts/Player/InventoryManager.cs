2f267cb3658c7e5026a786ffaf8347ee0e2b6d88
Arabaghsh <126906141+Arabaghsh@users.noreply.github.com>
2025-02-21 12:10:39 -0800

    Inventory System added

--- Assets/Scripts/Player/InventoryManager.cs
+++ Assets/Scripts/Player/InventoryManager.cs
@@ -2,21 +2,59 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-    public InventorySlot[] inventorySlots; 
+    public InventorySlot[] inventorySlots;
 
-    public void AddItem(Item item){
-        foreach(InventorySlot slot in inventorySlots){
-            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
-            if(itemInSlot == null){
-                SpawnNewItem(item, slot);
-                return;
+    public GameObject inventoryItemPrefab;
+
+    int selectedSlot = -1;
+
+    void Start (){
+        ChangeSelectedSlot(0);
+    }
+
+    void Update () {
+        if(Input.inputString != null){
+            bool isNum = int.TryParse(Input.inputString, out int number);
+            if(isNum && number > 0 && number < 10){
+                ChangeSelectedSlot(number - 1);
             }
         }
     }
 
+     void ChangeSelectedSlot(int newValue){
+        if (selectedSlot >=0){
+        inventorySlots[selectedSlot].Deselect();
+        }
+        inventorySlots[newValue].Select();
+        selectedSlot = newValue;
+     }     
+
+    public bool AddItem(Item item){
+
+        foreach(InventorySlot slot in inventorySlots){
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if(itemInSlot != null && itemInSlot.item == item && itemInSlot.count < 4  && itemInSlot.item.stackable == true){
+                itemInSlot.count++;
+                itemInSlot.RefreshCount();
+                return true;
+            }
+        }
+
+
+        foreach(InventorySlot slot in inventorySlots){
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if(itemInSlot == null){
+                SpawnNewItem(item, slot);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     void SpawnNewItem(Item item, InventorySlot slot){
-        // GameObject newItemGo = Instantiate(inventoryItemPrefab, slot.transform);
-        // InventoryItem inventoryItem = newItemGo.GetComponent<inventoryItem>();
-        // inventoryItem.InitialiseItem(item);
+        GameObject newItemGo = Instantiate(inventoryItemPrefab, slot.transform);
+        InventoryItem inventoryItem = newItemGo.GetComponent<InventoryItem>();
+        inventoryItem.InitialiseItem(item);
     }
 }
